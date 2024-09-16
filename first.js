6ad392cd65a73c76a8af4eeb0c8dbe7f44fe4026
Souhit <souhitpaul@gmail.com>
2024-09-16 20:37:54 +0530

    My first JavaScript Code
    Stone Paper Scissors GAME

---
+++ first.js
@@ -0,0 +1,13 @@
+console.log("Souhit Paul");
+fullname="Souhit Paul";
+age=24;
+x=null;
+Y=undefined;
+let name ="Venom";
+const Student={
+    fullNmae: "Souhit Paul",
+    Dept: "CSE",
+    rollNo: 51,
+    CGPA: 8.75,
+};
+//console.log(age)
